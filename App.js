91a9efc87676901c6069d8dd417e7ef081fa9dfa
uplsoumen <soumen.samanta@utplco.com>
2022-04-13 16:40:50 +0530

    Initial Init and tested in android

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
+import CameraModule from './CameraModule';
+const App = () => {
+  return (
+    <View style={{flex:1}}>
+      <CameraModule />
+    </View>
+  );
+};
+export default App;
